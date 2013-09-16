fe87f759e85d23a21500d4ee61b1520a3a924a5b
Joel Williamson <joel.s.williamson@gmail.com>
2013-09-16 14:24:47 -0400

    Added functions for all the various operations

--- processor.cpp
+++ processor.cpp
@@ -33,30 +33,75 @@ void Processor::changeProgramCounter(const int change)
 
 void Processor::exec()
 	{
+	// This is roughly the I stage
 	Word instruction = this->getWord(programCounter);
 	programCounter += 4;
 	char opcode = instruction >> 32-6; // This should get the upper six bits of the instruction
 	char funct = instruction & 0x3f; // Get the lower 6 bits in case this is a R format
 	
-	char rs = 0x03f80000 & instruction; 
-	char rt = 0x001f0000 & instruction;
+	Register rs = this->getRegister(0x03f80000 & instruction); 
+	Register rt = this->getRegister(0x001f0000 & instruction);
 	char im = 0x0000ffff & instruction;
-	char rd = 0x0000fc00 & instruction;
+	Register rd = this->getRegister(0x0000fc00 & instruction);
 	char sh = 0x000003c0 & instruction;
 
+	// E stage
 	switch (opcode)
 		{
 		case 0 : break;
-		case 2 : j(instruction|0x03ffffff); break;
-		case 3 : jal(instruction|0x03ffffff); break;
-		case 4 : beq(rs,rt,im); break;
-		case 5 : bne(rs,rt,im); break;
+		case 2 : this->j(instruction|0x03ffffff); break;
+		case 3 : this->jal(instruction|0x03ffffff); break;
+		case 4 : this->beq(rs,rt,im); break;
+		case 5 : this->bne(rs,rt,im); break;
 		case 6 : case 7 : break; // Not specified
-		case 8 : addi(rs,rt,im); break;
-		case 9 : addiu(rs,rt,im); break;
-		case 10: slti(rs,rt,im); break;
+		case 8 : this->addi(rs,rt,im); break;
+		case 9 : this->addiu(rs,rt,im); break;
+		case 10: this->slti(rs,rt,im); break;
 		case 11: break; // Not specified
-		case 12: andi(rs,rt,im); break;
-		case 13: ori(rs,rt,im); break;
+		case 12: this->andi(rs,rt,im); break;
+		case 13: this->ori(rs,rt,im); break;
 		case 14: break; // Not specified
-		case 15: lui(rs,rt,im); break;
+		case 15: this->lui(rs,rt,im); break;
+		
+		case 32: this->lb(rs,rt,im); break; // Load byte
+		case 33: this->lh(rs,rt,im); break; // Load halfword
+		case 34: break; // Not specified
+		case 35: this->lw(rs,rt,im); break;
+		case 36: this->lbu(rs,rt,im); break; // Load unsigned byte
+		case 37: this->lhu(rs,rt,im); break;
+		case 38: case 39: break; // Not specified
+		case 40: this->sb(rs,rt,im); break; // Store byte
+		case 41: this->sh(rs,rt,im); break;
+		case 42: break; // Not specified
+		case 43: this->sw(rs,rt,im); break;
+		default: break;
+		}
+	
+	if (!opcode)
+		{
+		switch (funct)
+			{
+			case 0 : this->sll(rs,rt,rd,sh); break;
+			case 1 : break; // Not specified
+			case 2 : this->srl(rs,rt,rd,sh); break;
+			case 3 : this->sra(rs,rt,rd,sh); break;
+			case 8 : this->jr(rs,rt,rd,sh); break;
+			case 16: this->mfhi(rs,rt,rd,sh); break;
+			case 18: this->mflo(rs,rt,rd,sh); break;
+			case 24: this->mult(rs,rt,rd,sh); break;
+			case 25: this->multu(rs,rt,rd,sh); break;
+			case 26: this->div(rs,rt,rd,sh); break;
+			case 27: this->divu(rs,rt,rd,sh); break;
+
+			case 32: this->add(rs,rt,rd,sh); break;
+			case 33: this->addu(rs,rt,rd,sh); break;
+			case 34: this->sub(rs,rt,rd,sh); break;
+			case 35: this->subu(rs,rt,rd,sh); break;
+			case 36: this->andd(rs,rt,rd,sh); break;
+			case 37: this->orr(rs,rt,rd,sh); break;
+			case 38: this->xorr(rs,rt,rd,sh); break;
+			case 39: this->nor(rs,rt,rd,sh); break;
+			case 42: this->slt(rs,rt,rd,sh); break;
+			}
+		}
+
--- processor.hpp
+++ processor.hpp
@@ -13,6 +13,8 @@ class Processor
 	std::vector <Word> mainMemory; // This should be larger, but will require a clever datastructure
 	Word programCounter;
 
+	Register hi,lo; // Registers for storing the results of certain operations
+
 public:
 	Processor(int startingAddress, const std::vector <Word>& initialMemory);
 
@@ -23,4 +25,44 @@ public:
 
 	void exec();
 
+	// Operations
+	void j(const Word address);
+	void jal(const Word address);
+	void beq(const Register rs, const Register rt, const Word im);
+	void bne(const Register rs, const Register rt, const Word im);
+	void addi(Register rs, const Register rt, const Word im) const;
+	void addiu(Register rs, const Register rt, const Word im) const;
+	void slti(Register rs, const Register rt, const Word im) const;
+	void andi(Register rs, const Register rt, const Word im) const;
+	void ori(Register rs, const Register rt, const Word im) const;
+	void lui(Register rs, const Register rt, const Word im) const;
+	void lb(Register rs, const Register rt, const Word im) const;
+	void lh(Register rs, const Register rt, const Word im) const;
+	void lw(Register rs, const Register rt, const Word im) const;
+	void lbu(Register rs, const Register rt, const Word im) const;
+	void lhu(Register rs, const Register rt, const Word im) const;
+	void sb(Register rs, const Register rt, const Word im) const;
+	void sh(Register rs, const Register rt, const Word im) const;
+	void sw(Register rs, const Register rt, const Word im) const;
+
+	// Functions
+	void sll(const Register rs, const Register rt, Register rd, const Word sh) const;
+	void srl(const Register rs, const Register rt, Register rd, const Word sh) const;
+	void sra(const Register rs, const Register rt, Register rd, const Word sh) const;
+	void jr(const Register rs, const Register rt, const Register rd, const Word sh);
+	void mfhi(const Register rs, const Register rt, Register rd, const Word sh) const;
+	void mflo(const Register rs, const Register rt, Register rd, const Word sh) const;
+	void mult(const Register rs, const Register rt, const Register rd, const Word sh);
+	void multu(const Register rs, const Register rt, const Register rd, const Word sh);
+	void div(const Register rs, const Register rt, const Register rd, const Word sh);
+	void divu(const Register rs, const Register rt, const Register rd, const Word sh);
+	void add(const Register rs, const Register rt, Register rd, const Word sh) const;
+	void addu(const Register rs, const Register rt, Register rd, const Word sh) const;
+	void sub(const Register rs, const Register rt, Register rd, const Word sh) const;
+	void subu(const Register rs, const Register rt, Register rd, const Word sh) const;
+	void andd(const Register rs, const Register rt, Register rd, const Word sh) const;
+	void orr(const Register rs, const Register rt, Register rd, const Word sh) const;
+	void xorr(const Register rs, const Register rt, Register rd, const Word sh) const;
+	void nor(const Register rs, const Register rt, Register rd, const Word sh) const;
+	void slt(const Register rs, const Register rt, Register rd, const Word sh) const;
 	};
