6ea4b76de0f2a6a4574cdd7d65e64cb884141771
Joel Williamson <joel.s.williamson@gmail.com>
2015-07-27 12:45:48 -0400

    Formatting and comments

--- processor.hpp
+++ processor.hpp
@@ -5,8 +5,6 @@
 
 #include <vector>
 
-
-
 /* The Processor class represents the state of a non-pipelined MIPS processor.
  * At any given instant, it has a program counter indicating which instruction
  * to execute next, a bank of 32 registers (each 32-bits wide) of easily
@@ -16,25 +14,35 @@
  * "Function" methods, which represent opcodes. To simulate a full run on a
  * program in memory, the `exec()` function will start execution at an address
  * specfied at construction time, and use real program flow.
+ *
+ * To implement interaction with off-processor resources (e.g. IO, control
+ * coprocessor, FPU, etc.), the processor will maintain a count of how many
+ * instructions it has executed. Periodically, it will yield to the caller,
+ * who can deal with any other resources. The caller than then reenter the
+ * processor execution, restarting the counter.
+ *
+ * For off-chip resources, probably a CRTP-style inheritance, with each
+ * resource maintaining a queue of signals, and the call handler can empty
+ * each queue when control is transferred to it.
  */
 class Processor
-	{
+{
 
-	Word programCounter;
-	std::vector <Register> registers; // The first register should be a zero-register
-	std::vector <Word> mainMemory; // This should be larger, but will require a clever datastructure
+  Word programCounter;
+  std::vector <Register> registers; // The first register should be a zero-register
+  std::vector <Word> mainMemory; // This should be larger, but will require a clever datastructure
 
-	Register hi,lo; // Registers for storing the results of certain operations
-
+  Register hi,lo; // Registers for storing the results of certain operations
 public:
-	Processor(int startingAddress, const std::vector <Word>& initialMemory);
+  Processor(int startingAddress, const std::vector <Word>& initialMemory);
 
-	Register& getRegister(const Word regNumber); 
-	Word& getWord(const Word address);
-	Word getProgramCounter() const;
-	void changeProgramCounter(const int change);
+  Register& getRegister(const Word regNumber);
+  Word& getWord(const Word address);
+  Word getProgramCounter() const;
+  void changeProgramCounter(const int change);
 
-	void exec();
+  // Step the processor one instruction
+  void exec();
 
 	// Operations
 	void j(const Word address);
